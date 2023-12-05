63a704934254e672d623b4f0f0595b5428544258
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2023-12-06 06:53:28 +0800

    add init details on tabs_functions

---
+++ tabs_functions/__init__.py
@@ -0,0 +1,6 @@
+## Initialize tabs_functions library
+## === This library contains the necessary modules to operate the APP ===##
+## === 1. appbasic_info: Basic Information About Your Sequence ===##
+## === 2.  appdot_plot: Dot Plot Sequence Alignment ===##
+## === 3. applocal_align: Local Alignment by Smith-Waterman Algorithm ===##
+## === 4. appglobal_align: Global Alignment by Needleman-Wunsch Algorithm ===##
