aa64aaca1a32f11988192f7ee0682f569de5bdf0
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2023-12-15 16:54:03 +0800

    Initialize local alignment connection to class SW Algorithm

--- tabs_functions/appglobal_align.py
+++ tabs_functions/appglobal_align.py
@@ -6,16 +6,62 @@ from ui.tabs.global_align_ui import Ui_Form
 
 class GlobalAlign(QWidget):
     def __init__(self):
-        super(GlobalAlign, self).__init__()
+        super(LocalAlign, self).__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        self.stylesheet_file("static/style/alignment_tab_style.qss")
+        self.stylesheet_file(style_path="static/style/alignment_tab_style.qss")
         QFontDatabase.addApplicationFont(
             ":/fonts/Lora-VariableFont/Lora-VariableFont_wght.ttf"
         )
 
+    #### ====== Signal Buttons  ======####
+    # self.ui.load_seq1.clciked.connect()
+    # self.ui.load_seq2.clciked.connect()
+    # self.ui.clear_btn.clciked.connect()
+    # self.ui.align_btn.clicked.connect(self.align_clicked)
+    # self.ui.save_btn.clciked.connect()
+
     def stylesheet_file(self, style_path):
-        style_path = "static/style/alignment_tab_style.qss"
         with open(style_path, "r") as f:
             style = f.read()
             self.setStyleSheet(style)
+
+    def align_clicked(self):
+        """Retrieving the input values upon clicking the Align button"""
+        #### ====== Retrieve Values ======####
+        label_1 = self.ui.seq1_label.text() or "No Sample Label Added [First Sequence]"
+        input_seq1 = self.ui.textEdit.toPlainText()
+        label_2 = self.ui.seq2_label.text() or "No Sample Label Added [First Sequence]"
+        input_seq2 = self.ui.textEdit.toPlainText()
+
+    def pop_warning(self, message):
+        """Warning Message if the Input is Wrong"""
+        pop_warning = QMessageBox()
+        pop_warning.setIcon(QMessageBox.Warning)
+        pop_warning.setWindowTitle("Input Error")
+        pop_warning.setText(message)
+        pop_warning.setStyleSheet(
+            """
+        QMessageBox{
+            background-color: rgb(54,54,54); /* charcoal gray*/ 
+            }
+        QMessageBox QLabel{
+            color: #fff;
+            }
+        QMessageBox QPushButton {
+            background-color: rgb(120,157,186); /* light blue */
+	        border: 3px solid rgb(5,92,142);
+	        border-radius:15px;
+	        padding: 5px;
+	        color: #000;
+            font-size:12px;
+            }
+        QMessageBox QPushButton::hover{
+            background-color: rgba(5,92,142,0.5); /* dark-blue */
+	        color: #fff;
+	        border-radius: 15px;
+            font-size:12px;
+            }
+        """
+        )
+        pop_warning.exec_()
--- tabs_functions/applocal_align.py
+++ tabs_functions/applocal_align.py
@@ -21,14 +21,75 @@ class LocalAlign(QWidget):
             ":/fonts/Lora-VariableFont/Lora-VariableFont_wght.ttf"
         )
 
-    #### ====== Signal Buttons  ======####
-    # self.ui.load_seq1.clciked.connect()
-    # self.ui.load_seq2.clciked.connect()
-    # self.ui.clear_btn.clciked.connect()
-    # self.ui.align_btn.clciked.connect()
-    # self.ui.save_btn.clciked.connect()
+        #### ====== Signal Buttons  ======####
+        # self.ui.load_seq1.clciked.connect()
+        # self.ui.load_seq2.clciked.connect()
+        # self.ui.clear_btn.clciked.connect()
+        self.ui.align_btn.clicked.connect(self.align_clicked)
+        # self.ui.save_btn.clciked.connect()
 
     def stylesheet_file(self, style_path):
         with open(style_path, "r") as f:
             style = f.read()
             self.setStyleSheet(style)
+
+    def align_clicked(self):
+        """Retrieving the input values upon clicking the Align button"""
+        #### ====== Retrieve Values ======####
+        label_1 = self.ui.seq1_label.text() or "No Sample Label Added [First Sequence]"
+        input_seq1 = self.ui.textEdit.toPlainText()
+        label_2 = self.ui.seq2_label.text() or "No Sample Label Added [First Sequence]"
+        input_seq2 = self.ui.textEdit.toPlainText()
+
+    def start_alignment(self, input_seq1, input_seq2, label_1, label_2):
+        """Implements the methods on class SmithWatermanAlgorithm"""
+        try:
+            self.localign = SmithWatermanAlgorithm(
+                scoring_sys, seqA=input_seq1, seqB=input_seq2
+            )
+            if self.localalign.is_valid:
+                result = self.localalign.result_alignment()
+            else:
+                raise ValueError()
+        except ValueError as e:
+            self.pop_warning(f"Local Alignment Error: {e}")
+
+    def result_alignment(sample_A, sample_B):
+        # Need to add instances
+        scoring_system = SWScoringSystem(match=2, mismatch=-1, gap=-1)
+        local_align = SmithWatermanAlgorithm(scoring_system)
+        aligned_seq_A, aligned_seq_B = local_align.seq_alignment(sample_A, sample_B)
+        percent_similarity_l = local_align.calc_similarity()
+        return aligned_seq_A, aligned_seq_B, percent_similarity_l
+
+    def pop_warning(self, message):
+        """Warning Message if the Input is Wrong"""
+        pop_warning = QMessageBox()
+        pop_warning.setIcon(QMessageBox.Warning)
+        pop_warning.setWindowTitle("Input Error")
+        pop_warning.setText(message)
+        pop_warning.setStyleSheet(
+            """
+        QMessageBox{
+            background-color: rgb(54,54,54); /* charcoal gray*/ 
+            }
+        QMessageBox QLabel{
+            color: #fff;
+            }
+        QMessageBox QPushButton {
+            background-color: rgb(120,157,186); /* light blue */
+	        border: 3px solid rgb(5,92,142);
+	        border-radius:15px;
+	        padding: 5px;
+	        color: #000;
+            font-size:12px;
+            }
+        QMessageBox QPushButton::hover{
+            background-color: rgba(5,92,142,0.5); /* dark-blue */
+	        color: #fff;
+	        border-radius: 15px;
+            font-size:12px;
+            }
+        """
+        )
+        pop_warning.exec_()
