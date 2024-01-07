9cc03abe2a5b9cc17cef2a72c7cb54eb55108063
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-01-07 17:41:44 +0800

    added library details

---
+++ seq_algorithm/__init__.py
@@ -0,0 +1,7 @@
+## Initialize seq_algorithm library
+## === This library contains the necessary modules for the pairwise sequence alignment feature of the APP ===##
+## === 1. dotplot: the algorithm for Dot Plot Alignment and Graphical Output ===##
+## === 2. global_needleman: Global Alignment Algorithm and Scoring System ===##
+## === 3. local_smithwaterman: Local Alignment Algorithm and Scoring System ===##
+## === 4. test: Just auxilliary script, to run those modules, used only during the development stage; not necessary when running the actual APP ===##
+## === 5. validate: the class for validating the user input for the two sequences before proceeding to the actual Alignment, raise errors to inform user ===##
