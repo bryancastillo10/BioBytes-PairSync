a7e71c9fe695cef4512a2f5222b2c6ea26cb37e4
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-01-07 17:34:25 +0800

    add comments for library details

---
+++ gene_toolkit/__init__.py
@@ -0,0 +1,5 @@
+## Initialize gene__toolkit library
+## === This library contains the necessary modules for the first feature/tab (basic information retrieval) ===##
+## === 1. bio_struct: contains the dictionary of biomolecules for reference of other modules ===##
+## === 2. parser: contains the class for allowing parsing data from NCBI ===##
+## === 3. seq_info: methods to retrieve the basic information/ parameters for DNA/RNA, and Protein Sequence ===##
